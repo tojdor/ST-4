6d095f365805fc598d4b1c8bad848df72178a928
tojdor <misram2005@gmail.com>
2026-05-15 23:18:06 +0300

    Initial commit

---
+++ BugPro/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Stateless;
+
+namespace BugPro;
+
+public enum BugState
+{
+    New,
+    Triage,
+    Fixing,
+    Verification,
+    Closed,
+    Returned,
+    Reopened,
+    NeedInfo,
+    OtherProduct,
+    NeedDecisionLater,
+    NotABug,
+    WontFix,
+    Duplicate,
+    NotReproducible
+}
+
+public enum BugTrigger
+{
+    StartTriage,
+    StartFix,
+    MarkFixed,
+    Approve,
+    Reject,
+    Return,
+    Reopen,
+    NeedInfo,
+    OtherProduct,
+    NeedDecisionLater,
+    MarkNotBug,
+    MarkWontFix,
+    MarkDuplicate,
+    MarkNotRepro
+}
+
+public class Bug
+{
+    private readonly StateMachine<BugState, BugTrigger> _machine;
+
+    public BugState State => _machine.State;
+
+    public Bug(BugState initialState = BugState.New)
+    {
+        _machine = new StateMachine<BugState, BugTrigger>(initialState);
+        Configure();
+    }
+
+    public bool CanFire(BugTrigger trigger) => _machine.CanFire(trigger);
+
+    public void Fire(BugTrigger trigger) => _machine.Fire(trigger);
+
+    public void StartTriage() => _machine.Fire(BugTrigger.StartTriage);
+
+    public void StartFix() => _machine.Fire(BugTrigger.StartFix);
+
+    public void MarkFixed() => _machine.Fire(BugTrigger.MarkFixed);
+
+    public void Approve() => _machine.Fire(BugTrigger.Approve);
+
+    public void Reject() => _machine.Fire(BugTrigger.Reject);
+
+    public void Return() => _machine.Fire(BugTrigger.Return);
+
+    public void Reopen() => _machine.Fire(BugTrigger.Reopen);
+
+    public void NeedInfo() => _machine.Fire(BugTrigger.NeedInfo);
+
+    public void OtherProduct() => _machine.Fire(BugTrigger.OtherProduct);
+
+    public void NeedDecisionLater() => _machine.Fire(BugTrigger.NeedDecisionLater);
+
+    public void MarkNotBug() => _machine.Fire(BugTrigger.MarkNotBug);
+
+    public void MarkWontFix() => _machine.Fire(BugTrigger.MarkWontFix);
+
+    public void MarkDuplicate() => _machine.Fire(BugTrigger.MarkDuplicate);
+
+    public void MarkNotRepro() => _machine.Fire(BugTrigger.MarkNotRepro);
+
+    private void Configure()
+    {
+        _machine.Configure(BugState.New)
+            .Permit(BugTrigger.StartTriage, BugState.Triage);
+
+        _machine.Configure(BugState.Triage)
+            .Permit(BugTrigger.StartFix, BugState.Fixing)
+            .Permit(BugTrigger.NeedInfo, BugState.NeedInfo)
+            .Permit(BugTrigger.OtherProduct, BugState.OtherProduct)
+            .Permit(BugTrigger.NeedDecisionLater, BugState.NeedDecisionLater)
+            .Permit(BugTrigger.MarkNotBug, BugState.NotABug)
+            .Permit(BugTrigger.MarkWontFix, BugState.WontFix)
+            .Permit(BugTrigger.MarkDuplicate, BugState.Duplicate)
+            .Permit(BugTrigger.MarkNotRepro, BugState.NotReproducible);
+
+        _machine.Configure(BugState.NeedInfo)
+            .Permit(BugTrigger.Return, BugState.Returned);
+        _machine.Configure(BugState.OtherProduct)
+            .Permit(BugTrigger.Return, BugState.Returned);
+        _machine.Configure(BugState.NeedDecisionLater)
+            .Permit(BugTrigger.Return, BugState.Returned);
+        _machine.Configure(BugState.NotABug)
+            .Permit(BugTrigger.Return, BugState.Returned);
+        _machine.Configure(BugState.WontFix)
+            .Permit(BugTrigger.Return, BugState.Returned);
+        _machine.Configure(BugState.Duplicate)
+            .Permit(BugTrigger.Return, BugState.Returned);
+        _machine.Configure(BugState.NotReproducible)
+            .Permit(BugTrigger.Return, BugState.Returned);
+
+        _machine.Configure(BugState.Returned)
+            .Permit(BugTrigger.StartTriage, BugState.Triage);
+
+        _machine.Configure(BugState.Fixing)
+            .Permit(BugTrigger.MarkFixed, BugState.Verification);
+
+        _machine.Configure(BugState.Verification)
+            .Permit(BugTrigger.Approve, BugState.Closed)
+            .Permit(BugTrigger.Reject, BugState.Returned);
+
+        _machine.Configure(BugState.Closed)
+            .Permit(BugTrigger.Reopen, BugState.Reopened);
+
+        _machine.Configure(BugState.Reopened)
+            .Permit(BugTrigger.StartTriage, BugState.Triage);
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        var bug = new Bug();
+        Console.WriteLine($"Initial: {bug.State}");
+
+        bug.StartTriage();
+        Console.WriteLine($"After StartTriage: {bug.State}");
+
+        bug.StartFix();
+        Console.WriteLine($"After StartFix: {bug.State}");
+
+        bug.MarkFixed();
+        Console.WriteLine($"After MarkFixed: {bug.State}");
+
+        bug.Approve();
+        Console.WriteLine($"After Approve: {bug.State}");
+
+        bug.Reopen();
+        Console.WriteLine($"After Reopen: {bug.State}");
+
+        bug.StartTriage();
+        Console.WriteLine($"After StartTriage (Reopened): {bug.State}");
+    }
+}
---
+++ BugTests/UnitTest1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+
+namespace BugTests;
+
+[TestClass]
+public class BugWorkflowTests
+{
+    [TestMethod]
+    public void InitialStateIsNew()
+    {
+        var bug = new Bug();
+        Assert.AreEqual(BugState.New, bug.State);
+    }
+
+    [TestMethod]
+    public void StartTriageFromNewMovesToTriage()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        Assert.AreEqual(BugState.Triage, bug.State);
+    }
+
+    [TestMethod]
+    public void StartFixFromTriageMovesToFixing()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.StartFix();
+        Assert.AreEqual(BugState.Fixing, bug.State);
+    }
+
+    [TestMethod]
+    public void NeedInfoFromTriageMovesToNeedInfo()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.NeedInfo();
+        Assert.AreEqual(BugState.NeedInfo, bug.State);
+    }
+
+    [TestMethod]
+    public void OtherProductFromTriageMovesToOtherProduct()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.OtherProduct();
+        Assert.AreEqual(BugState.OtherProduct, bug.State);
+    }
+
+    [TestMethod]
+    public void NeedDecisionLaterFromTriageMovesToNeedDecisionLater()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.NeedDecisionLater();
+        Assert.AreEqual(BugState.NeedDecisionLater, bug.State);
+    }
+
+    [TestMethod]
+    public void MarkNotBugFromTriageMovesToNotABug()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.MarkNotBug();
+        Assert.AreEqual(BugState.NotABug, bug.State);
+    }
+
+    [TestMethod]
+    public void MarkWontFixFromTriageMovesToWontFix()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.MarkWontFix();
+        Assert.AreEqual(BugState.WontFix, bug.State);
+    }
+
+    [TestMethod]
+    public void MarkDuplicateFromTriageMovesToDuplicate()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.MarkDuplicate();
+        Assert.AreEqual(BugState.Duplicate, bug.State);
+    }
+
+    [TestMethod]
+    public void MarkNotReproFromTriageMovesToNotReproducible()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.MarkNotRepro();
+        Assert.AreEqual(BugState.NotReproducible, bug.State);
+    }
+
+    [TestMethod]
+    public void ReturnFromNeedInfoMovesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.NeedInfo();
+        bug.Return();
+        Assert.AreEqual(BugState.Returned, bug.State);
+    }
+
+    [TestMethod]
+    public void ReturnFromOtherProductMovesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.OtherProduct();
+        bug.Return();
+        Assert.AreEqual(BugState.Returned, bug.State);
+    }
+
+    [TestMethod]
+    public void ReturnFromNeedDecisionLaterMovesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.NeedDecisionLater();
+        bug.Return();
+        Assert.AreEqual(BugState.Returned, bug.State);
+    }
+
+    [TestMethod]
+    public void ReturnFromNotABugMovesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.MarkNotBug();
+        bug.Return();
+        Assert.AreEqual(BugState.Returned, bug.State);
+    }
+
+    [TestMethod]
+    public void ReturnFromWontFixMovesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.MarkWontFix();
+        bug.Return();
+        Assert.AreEqual(BugState.Returned, bug.State);
+    }
+
+    [TestMethod]
+    public void ReturnFromDuplicateMovesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.MarkDuplicate();
+        bug.Return();
+        Assert.AreEqual(BugState.Returned, bug.State);
+    }
+
+    [TestMethod]
+    public void ReturnFromNotReproMovesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.MarkNotRepro();
+        bug.Return();
+        Assert.AreEqual(BugState.Returned, bug.State);
+    }
+
+    [TestMethod]
+    public void StartTriageFromReturnedMovesToTriage()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.NeedInfo();
+        bug.Return();
+        bug.StartTriage();
+        Assert.AreEqual(BugState.Triage, bug.State);
+    }
+
+    [TestMethod]
+    public void MarkFixedFromFixingMovesToVerification()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.StartFix();
+        bug.MarkFixed();
+        Assert.AreEqual(BugState.Verification, bug.State);
+    }
+
+    [TestMethod]
+    public void ApproveFromVerificationMovesToClosed()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.StartFix();
+        bug.MarkFixed();
+        bug.Approve();
+        Assert.AreEqual(BugState.Closed, bug.State);
+    }
+
+    [TestMethod]
+    public void RejectFromVerificationMovesToReturned()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.StartFix();
+        bug.MarkFixed();
+        bug.Reject();
+        Assert.AreEqual(BugState.Returned, bug.State);
+    }
+
+    [TestMethod]
+    public void ReopenFromClosedMovesToReopened()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.StartFix();
+        bug.MarkFixed();
+        bug.Approve();
+        bug.Reopen();
+        Assert.AreEqual(BugState.Reopened, bug.State);
+    }
+
+    [TestMethod]
+    public void StartTriageFromReopenedMovesToTriage()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        bug.StartFix();
+        bug.MarkFixed();
+        bug.Approve();
+        bug.Reopen();
+        bug.StartTriage();
+        Assert.AreEqual(BugState.Triage, bug.State);
+    }
+
+    [TestMethod]
+    public void ApproveInNewThrows()
+    {
+        var bug = new Bug();
+        Assert.ThrowsException<InvalidOperationException>(() => bug.Approve());
+    }
+
+    [TestMethod]
+    public void ReturnInTriageThrows()
+    {
+        var bug = new Bug();
+        bug.StartTriage();
+        Assert.ThrowsException<InvalidOperationException>(() => bug.Return());
+    }
+}
